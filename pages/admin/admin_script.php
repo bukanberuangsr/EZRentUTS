7940d23c5b9371b63f61217109af9fb951b93b83
Akhtar Hafiz P <akhfizpuli@gmail.com>
2024-10-17 22:50:18 +0700

    Added basic user guide

--- pages/admin/admin_script.php
+++ pages/admin/admin_script.php
@@ -1,12 +1,17 @@
-<!-- <?php
-// include './db/db.php';
-// $username = "admin";
-// $password = password_hash('admin123', PASSWORD_DEFAULT);
-// $is_admin = 1;
-// $query = "INSERT INTO users (username, userpass, is_admin) VALUES ('$username','$password','$is_admin')";
-// if (mysqli_query($conn, $query)) {
-//     echo "Admin Account has been created";
-// } else {
-//     echo "Error: " . mysqli_error($conn);
-// }
-?> -->
+<!--
+ This script is used to create an admin account.
+ To use it, simply open this file with a web browser.
+ Be sure to start your web server and database.
+-->
+<?php
+include './db/db.php';
+$username = "admin";
+$password = password_hash('admin123', PASSWORD_DEFAULT);
+$is_admin = 1;
+$query = "INSERT INTO users (username, userpass, is_admin) VALUES ('$username','$password','$is_admin')";
+if (mysqli_query($conn, $query)) {
+    echo "Admin Account has been created";
+} else {
+    echo "Error: " . mysqli_error($conn);
+}
+?>
